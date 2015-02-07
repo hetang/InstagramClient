b89d4499de70bd93da8d6229d0c6803abc6aa154
Hetang Shah <hetang.shah@gmail.com>
2015-02-07 03:19:08 -0800

    Customizing List View

--- app/src/main/java/com/air/instagramclient/Adaptors/InstagramPhotosAdaptor.java
+++ app/src/main/java/com/air/instagramclient/Adaptors/InstagramPhotosAdaptor.java
@@ -1,6 +1,11 @@
 package com.air.instagramclient.Adaptors;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Typeface;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +15,7 @@ import android.widget.TextView;
 
 import com.air.instagramclient.R;
 import com.air.instagramclient.models.InstagramPhotoModel;
+import com.air.instagramclient.models.UserModel;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
@@ -23,6 +29,7 @@ public class InstagramPhotosAdaptor extends ArrayAdapter<InstagramPhotoModel>{
     private static class ViewHolder{
         ImageView imgVwPhoto;
         TextView txtVwCaption;
+        TextView txtVwLikes;
     }
 
     public InstagramPhotosAdaptor(Context context, List<InstagramPhotoModel> objects) {
@@ -35,17 +42,43 @@ public class InstagramPhotosAdaptor extends ArrayAdapter<InstagramPhotoModel>{
         ViewHolder viewHolder;
         if(convertView == null) {
             viewHolder = new ViewHolder();
+            Typeface fontMagnificent = Typeface.createFromAsset(getContext().getAssets(), "fonts/Magnificent.ttf");
+            Typeface fontRingm = Typeface.createFromAsset(getContext().getAssets(), "fonts/RINGM.ttf");
+
+            Drawable dr = getContext().getResources().getDrawable(R.drawable.heart1);
+            Bitmap bitmap = ((BitmapDrawable) dr).getBitmap();
+            // Scale it to 50 x 50
+            Drawable d = new BitmapDrawable(getContext().getResources(), Bitmap.createScaledBitmap(bitmap, 45, 45, true));
+            int h = d.getIntrinsicHeight();
+            int w = d.getIntrinsicWidth();
+            d.setBounds( 0, 0, w, h );
+
             // Do not attach to parent yet. Hence, we are passing false here
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_photo,
                                                                     parent, false);
 
             viewHolder.imgVwPhoto = (ImageView) convertView.findViewById(R.id.imgVwPhoto);
             viewHolder.txtVwCaption = (TextView) convertView.findViewById(R.id.txtVwCaption);
+            viewHolder.txtVwCaption.setTypeface(fontMagnificent);
+            viewHolder.txtVwLikes = (TextView) convertView.findViewById(R.id.txtVwLikes);
+            viewHolder.txtVwLikes.setTypeface(fontRingm);
+            viewHolder.txtVwLikes.setCompoundDrawables( d, null, null, null );
+            viewHolder.txtVwLikes.setCompoundDrawablePadding(10);
+
             convertView.setTag(viewHolder);
         } else {
             viewHolder = (ViewHolder) convertView.getTag();
         }
-        viewHolder.txtVwCaption.setText(model.getCaption());
+        StringBuilder userName = new StringBuilder();
+        UserModel user = model.getUser();
+        if(user != null) {
+            userName.append("<b>").append(model.getUser().getName()).append("</b> ");
+        }
+        userName.append(model.getCaption());
+
+        viewHolder.txtVwCaption.setText(Html.fromHtml(userName.toString()));
+        viewHolder.txtVwLikes.setText(Html.fromHtml("<b>" + model.getLikes() + "</b> " + getContext().getResources().getString(R.string.likes_string)));
+
         viewHolder.imgVwPhoto.setImageResource(0);
         Picasso.with(getContext()).load(model.getImageURL()).fit().centerCrop().placeholder(R.drawable.loading1).into(viewHolder.imgVwPhoto);
 
